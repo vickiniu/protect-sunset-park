68fb1869153de574f055ce7e6807c22f61e39292
System <none>
2017-02-07 20:59:06 +0000

    Checkpoint

---
+++ public/client.js
@@ -0,0 +1,8 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+// add other scripts at the bottom of index.html
+
+(function() {
+  console.log('time 2 javascript')
+})();
